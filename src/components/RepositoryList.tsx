6536eaf8c0433d2cc2a1d4a1465b67dbcf456a92
Lucas R Pinto <lucspinto@outlook.com>
2022-05-29 00:47:46 -0300

    Final

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -4,8 +4,14 @@ import '../styles/repositories.scss';
 
 //https://api.github.com/users/lucrybpin/repos
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
      fetch('https://api.github.com/users/lucrybpin/repos')
@@ -23,7 +29,6 @@ export function RepositoryList() {
             return <RepositoryItem key={repository.name} repository={repository}/>
           }
         )}
-        
       </ul>
     </section>
   );
